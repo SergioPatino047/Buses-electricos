8ac5c417b8f588c532a0e0c1eeb0fc6497894b68
Sergio_Rodriguez <sergio.patino@correounivalle.edu.co>
2026-07-01 17:52:17 -0500

    Add time delay in fill_module, charger management, and thread-based bus logic

--- Buses.py
+++ Buses.py
@@ -1,14 +1,29 @@
 import threading 
+import time
 
 def bus(n: int) -> list[int]:
     modulos: list[int] = [0] * n
     return modulos
 
-def fill_module(bus: list[int]):
-    for i in range(len(bus)):
-        while bus[i] < 100:
-            bus[i] += 1
+def fill_module(bus: list[int],i:int):
+    while bus[i] < 100:
+        bus[i] += 1
+        print(bus)
+        time.sleep(1)
     return bus
-        
 
-print(fill_module(bus(5)))
+def chargers(k: int) -> list[int]:
+    cargadores: list[int] = [1] * k
+    return cargadores
+
+def thread_function(bus: list[int], chargers: list[int]):
+    while True:
+        for i in range(len(chargers)):
+            if chargers[i] == 1:
+                chargers[i] = 0
+                for j in range(len(bus)):
+                    fill_module(bus, j)
+                chargers[i] = 1
+                return bus
+        print("No charger available, waiting...")
+        time.sleep(5)
