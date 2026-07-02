ac6035b96fe6217e07c3d9ea48df32d032bc9315
Sergio_Rodriguez <sergio.patino@correounivalle.edu.co>
2026-07-01 22:46:14 -0500

    Delete barrier for entry of continous buses

--- Buses.py
+++ Buses.py
@@ -14,45 +14,29 @@ def fill_module(bus: list[int],i:int):
         time.sleep(0.8)
     return bus
 
-def thread_function(bus: list[int], barrier: threading.Barrier, semaphore: threading.Semaphore):
-
-    barrier.wait()
+def thread_function(bus: list[int]):
 
     semaphore.acquire()
 
     print(f"[{threading.current_thread().name}] started charging")
-
     for j in range(len(bus)):
         fill_module(bus, j)
-
     print(f"[{threading.current_thread().name}] finished charging")
 
     semaphore.release()
 
     return bus
 
-def threads_buses(b: int, n: int, k: int):
+def threads_buses(n: int):
 
-    barrier = threading.Barrier(b)
+    bus_instance = bus(n)
+    
+    thread = threading.Thread(
+        target=thread_function,
+        args=(bus_instance,)
+    )
+    return thread
+
+def num_cargadores(k: int):
+    global semaphore
     semaphore = threading.Semaphore(k)
-
-    if b > k:
-        bus_list = [bus(n) for _ in range(b)]
-        threads_list = []
-
-        for i in range(b):
-            thread = threading.Thread(
-                target=thread_function,
-                args=(bus_list[i], barrier, semaphore),
-                name=f"Bus-{i+1}"
-            )
-
-            threads_list.append(thread)
-            thread.start()
-
-        for thread in threads_list:
-            thread.join()
-    else:
-        print("The number of buses must be greater than the number of charging stations.")
-
-threads_buses(5, 5, 6)
