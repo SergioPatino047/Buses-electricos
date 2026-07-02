4d38b105aefd697e9c8995d55783f010ccf6cc76
Sergio_Rodriguez <sergio.patino@correounivalle.edu.co>
2026-07-01 23:35:39 -0500

    Implement Queue FIFO for continous Buses

--- Buses.py
+++ Buses.py
@@ -1,7 +1,10 @@
-from concurrent.futures import thread
+from queue import Queue
 import threading
 import time
 
+def num_cargadores(k: int):
+    global semaphore
+    semaphore = threading.Semaphore(k)
 
 def bus(n: int) -> list[int]:
     modulos: list[int] = [0] * n
@@ -9,34 +12,59 @@ def bus(n: int) -> list[int]:
 
 def fill_module(bus: list[int],i:int):
     while bus[i] < 100:
-        bus[i] += 50
+        bus[i] += 15
+        if bus[i] > 100:
+            bus[i] = 100
         print(f"[{threading.current_thread().name}] Bus: {bus}")
         time.sleep(0.8)
     return bus
 
-def thread_function(bus: list[int]):
+def thread_function(bus):
 
     semaphore.acquire()
 
     print(f"[{threading.current_thread().name}] started charging")
-    for j in range(len(bus)):
-        fill_module(bus, j)
+    for i in range(len(bus)):
+        fill_module(bus, i)
     print(f"[{threading.current_thread().name}] finished charging")
 
     semaphore.release()
 
-    return bus
+def threads_buses(num_modules, bus_id):
 
-def threads_buses(n: int):
+    bus_instance = bus(num_modules)
 
-    bus_instance = bus(n)
-    
-    thread = threading.Thread(
+    return threading.Thread(
         target=thread_function,
-        args=(bus_instance,)
+        args=(bus_instance,),
+        name=f"Bus-{bus_id}"
     )
-    return thread
 
-def num_cargadores(k: int):
-    global semaphore
-    semaphore = threading.Semaphore(k)
+def generate_buses(num_modules):
+    bus_id = 1
+    while True:
+        thread = threads_buses(num_modules, bus_id)
+        bus_queue.put(thread)
+        print(f"[Generator] Bus-{bus_id} arrived.")
+        bus_id += 1
+        time.sleep(5)  
+
+def dispatcher():
+
+    while True:
+
+        thread = bus_queue.get()
+        thread.start()
+        bus_queue.task_done()
+
+num_cargadores(5)
+bus_queue = Queue()
+
+dispatcher_thread = threading.Thread(
+    target=dispatcher,
+    daemon=True
+)
+
+dispatcher_thread.start()
+
+generate_buses(5)
