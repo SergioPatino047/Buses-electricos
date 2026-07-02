235878156f60ae4e488a22457390bdb7208531de
Sergio_Rodriguez <sergio.patino@correounivalle.edu.co>
2026-07-01 22:02:54 -0500

    Implement semaphore, barrier and threads function

--- Buses.py
+++ Buses.py
@@ -3,9 +3,6 @@ import threading
 import time
 
 
-barrier = threading.Barrier(3)
-
-
 def bus(n: int) -> list[int]:
     modulos: list[int] = [0] * n
     return modulos
@@ -13,55 +10,49 @@ def bus(n: int) -> list[int]:
 def fill_module(bus: list[int],i:int):
     while bus[i] < 100:
         bus[i] += 50
-        print(f"[{threading.current_thread().name}], CURRENTBUS: {bus}")
+        print(f"[{threading.current_thread().name}] Bus: {bus}")
+        time.sleep(0.8)
     return bus
 
-def chargers(k: int) -> list[int]:
-    cargadores: list[int] = [1] * k
-    return cargadores
+def thread_function(bus: list[int], barrier: threading.Barrier, semaphore: threading.Semaphore):
 
-def thread_function(bus: list[int], chargers: list[int]):
+    barrier.wait()
 
+    semaphore.acquire()
 
-    barrier.wait()  
-    
+    print(f"[{threading.current_thread().name}] started charging")
 
-    while True:
-        for i in range(len(chargers)):
+    for j in range(len(bus)):
+        fill_module(bus, j)
 
-            if chargers[i] == 1:
-                chargers[i] = 0
-                print(f"[{threading.current_thread().name}] acquired charger {i}")
-                for j in range(len(bus)):
-                    if bus[j] != 100:
-                        fill_module(bus, j)
-                        break
-                chargers[i] = 1
-                print(f"[{threading.current_thread().name}] released charger {i}")
-                if all(module == 100 for module in bus):
-                    print(f"[{threading.current_thread().name}] All modules are fully charged.")
-                    return bus
+    print(f"[{threading.current_thread().name}] finished charging")
 
-                
-        print(f"No charger available, [{threading.current_thread().name}] is waiting...")
-        time.sleep(5)
+    semaphore.release()
 
-bus1 = bus(2)
-print("Bus 1 modules:", bus1)
-bus2 = bus(2)
-print("Bus 2 modules:", bus2)
-bus3 = bus(2)
-print("Bus 3 modules:", bus3)
-chargers_list = chargers(1)
-print("Chargers:", chargers_list)
-thread1 = threading.Thread(target=thread_function, args=(bus1, chargers_list))
-thread2 = threading.Thread(target=thread_function, args=(bus2, chargers_list))
-thread3 = threading.Thread(target=thread_function, args=(bus3, chargers_list))
-thread1.start()
-thread2.start()
-thread3.start()
-thread1.join()
-thread2.join()
-thread3.join()
+    return bus
 
+def threads_buses(b: int, n: int, k: int):
 
+    barrier = threading.Barrier(b)
+    semaphore = threading.Semaphore(k)
+
+    if b > k:
+        bus_list = [bus(n) for _ in range(b)]
+        threads_list = []
+
+        for i in range(b):
+            thread = threading.Thread(
+                target=thread_function,
+                args=(bus_list[i], barrier, semaphore),
+                name=f"Bus-{i+1}"
+            )
+
+            threads_list.append(thread)
+            thread.start()
+
+        for thread in threads_list:
+            thread.join()
+    else:
+        print("The number of buses must be greater than the number of charging stations.")
+
+threads_buses(5, 5, 6)
